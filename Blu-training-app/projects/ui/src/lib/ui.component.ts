5952d72f9663fdaac7cba22933e6e825899dbfe0
Hiba Hyani <lhen.alamal@gmail.com>
2024-03-24 23:39:00 +0200

    create lib ui and add component table

---
+++ Blu-training-app/projects/ui/src/lib/ui.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ui works!
+      Hello World i am ui library!
+    </p>
+  `,
+  styles: ``
+})
+export class UiComponent {
+
+}

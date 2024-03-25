42ece39d0d4f0a2e045efc6b78c6b5e3a9f9ca0d
Hiba Hyani <lhen.alamal@gmail.com>
2024-03-25 15:56:25 +0200

    create simple table

--- Blu-training-app/projects/ui/table/table.component.ts
+++ Blu-training-app/projects/ui/table/table.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './table.component.css'
 })
 export class TableComponent {
+    users : any[]= [
+    {
+      "userid":1,
+      "Firstname":"Joe",
+      "Lastname":"Doe",
+      "Email":"jo@gmail.com "  
+    },
+    {
+      "userid":2,
+      "Firstname":"Mary",
+      "Lastname":"Moe",
+      "Email":"mo@gmail.com "  
+    },{
+      "userid":3,
+      "Firstname":"July",
+      "Lastname":"doo",
+      "Email":"julygmail.com "  
+    }
+
+  ]
 
 }
--- Blu-training-app/src/app/app.component.ts
+++ Blu-training-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UiComponent } from "../../projects/ui/src/lib/ui.component";
+import { TableComponent } from '../../projects/ui/table/table.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, UiComponent]
+    imports: [RouterOutlet, UiComponent,TableComponent]
 })
 export class AppComponent {
   title = 'Blu-training-app';
